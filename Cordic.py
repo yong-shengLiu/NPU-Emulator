Fix softmax accuracy problem

Cordic.py
@@ -10,6 +10,7 @@ def cordic(x, y, theta, m, iterations=1, mode='circular'):
     (2) m=0,  rotation mode(V), vectoring mode(V)   (linear)
     (3) m=-1, rotation mode(V), vectoring mode(V)   (hyperbolic)
     """
+    threshold = 1e-20
     hyperbolic_iteration = [
         1, 2, 3, 4, 4, 5, 6, 7, 8, 9,
         10, 11, 12, 13, 13, 14, 15, 16, 17, 18,
@@ -40,7 +41,7 @@ def cordic(x, y, theta, m, iterations=1, mode='circular'):
     elif m == -1:
         for i in hyperbolic_iteration[:iterations]:
             K *= 1 / math.sqrt(1 - 2**(-2*i))
-
+            
     # Perform iterative rotation
     if m == -1:
         for i in range(iterations):
@@ -60,14 +61,26 @@ def cordic(x, y, theta, m, iterations=1, mode='circular'):
             x, y = x_new, y_new
     else:
         for i in range(iterations):
+            # Early termination: y is already close enough to 0
             if mode == "rotation":
                 di = 1.0 if theta >= 0 else -1.0
                 convergence_list.append(theta)
+                
+                if abs(theta) < threshold:
+                    print(f"Early stop at iteration {i}, theta ≈ 0")
+                    break
+                
             elif mode == "vectoring":
                 di = -1.0 if y >= 0 else 1.0
                 convergence_list.append(y)
+                
+                if abs(y) < threshold:
+                    print(f"Early stop at iteration {i}, y ≈ 0")
+                    break
+                
             else:
                 raise ValueError("Invalid mode")
+
 
             x_new = x - m * di * y * 2**(-i)
             y_new = y + di * x * 2**(-i)
@@ -86,96 +99,102 @@ def cordic(x, y, theta, m, iterations=1, mode='circular'):
 if __name__ == "__main__":
     print("=== Cordic testbench ===")
 
-    ## ---- Circular rotation mode ---- ##
-    print(f"\n\n<Case1> Circular rotation mode")
-    # cos sin
-    cos_cordic, sin_cordic, theta_remain, convergence_list = cordic(1, 0, math.pi/4, m=1, iterations=32, mode='rotation')
-    print(f"cos: {cos_cordic}, sin: {sin_cordic}, theta: {theta_remain}")
+    # ## ---- Circular rotation mode ---- ##
+    # print(f"\n\n<Case1> Circular rotation mode")
+    # # cos sin
+    # cos_cordic, sin_cordic, theta_remain, convergence_list = cordic(1, 0, math.pi/4, m=1, iterations=32, mode='rotation')
+    # print(f"cos: {cos_cordic}, sin: {sin_cordic}, theta: {theta_remain}")
 
-    # True values
-    cos_true = math.cos(math.pi/4)
-    sin_true = math.sin(math.pi/4)
-
-
-    # Compute errors
-    cos_error = abs(cos_cordic - cos_true)
-    sin_error = abs(sin_cordic - sin_true)
-
-    print(f"Error:  cos error={cos_error}, sin error={sin_error}")
+    # # True values
+    # cos_true = math.cos(math.pi/4)
+    # sin_true = math.sin(math.pi/4)
 
 
-    ## ---- Circular vectoring mode ---- ##
-    print(f"\n\n<Case2> Circular vectoring mode")
-    # square(x^2 + y^2), tan-1(y)
-    square_cordic, y_remain, arctan_cordic, convergence_list = cordic(1, 1, 0, m=1, iterations=32, mode='vectoring')
-    print(f"square: {square_cordic}, y: {y_remain}, arctan: {arctan_cordic}")
+    # # Compute errors
+    # cos_error = abs(cos_cordic - cos_true)
+    # sin_error = abs(sin_cordic - sin_true)
 
-    square_true = math.sqrt(2)  # sqrt(1^2 + 1^2)
-    arctan_true = math.atan(1)      # 0.785... ≈ π/4
-
-    square_error = abs(square_cordic - square_true)
-    arctan_error = abs(arctan_cordic - arctan_true)
-    print(f"Error:  square error={square_error}, arctan error={arctan_error}")
+    # print(f"Error:  cos error={cos_error}, sin error={sin_error}")
 
 
-    ## ---- Linear rotation mode ---- ##
-    print(f"\n\n<Case3> Linear rotation mode")
-    # y + xz
-    x_cordic, acc_cordic, theta_remain, convergence_list = cordic(2.3, 4.3, 1.4, m=0, iterations=32, mode='rotation')
-    print(f"x: {x_cordic}, acc: {acc_cordic}, theta: {theta_remain}")
+    # ## ---- Circular vectoring mode ---- ##
+    # print(f"\n\n<Case2> Circular vectoring mode")
+    # # square(x^2 + y^2), tan-1(y)
+    # square_cordic, y_remain, arctan_cordic, convergence_list = cordic(1, 1, 0, m=1, iterations=32, mode='vectoring')
+    # print(f"square: {square_cordic}, y: {y_remain}, arctan: {arctan_cordic}")
 
-    acc_true = 4.3 + 2.3 * 1.4
+    # square_true = math.sqrt(2)  # sqrt(1^2 + 1^2)
+    # arctan_true = math.atan(1)      # 0.785... ≈ π/4
 
-    acc_error = abs(acc_cordic - acc_true)
-    print(f"Error:  acc error={acc_error}")
-
-
-    ## ---- Linear vectoring mode ---- ##
-    print(f"\n\n<Case4> Linear vectoring mode")
-    # z + y/x
-    x_cordic, y_remain, div_cordic, convergence_list = cordic(2.3, 4.3, 1.4, m=0, iterations=32, mode='vectoring')
-    print(f"x: {x_cordic}, y: {y_remain}, div: {div_cordic}")
-
-    div_true = 1.4 + 4.3 / 2.3
-
-    div_error = abs(div_cordic - div_true)
-    print(f"Error:  div error={div_error}")
+    # square_error = abs(square_cordic - square_true)
+    # arctan_error = abs(arctan_cordic - arctan_true)
+    # print(f"Error:  square error={square_error}, arctan error={arctan_error}")
 
 
-    ## ---- Hyperbolic rotation mode ---- ##
+    # ## ---- Linear rotation mode ---- ##
+    # print(f"\n\n<Case3> Linear rotation mode")
+    # # y + xz
+    # x_cordic, acc_cordic, theta_remain, convergence_list = cordic(2.3, 4.3, 1.4, m=0, iterations=32, mode='rotation')
+    # print(f"x: {x_cordic}, acc: {acc_cordic}, theta: {theta_remain}")
+
+    # acc_true = 4.3 + 2.3 * 1.4
+
+    # acc_error = abs(acc_cordic - acc_true)
+    # print(f"Error:  acc error={acc_error}")
+
+
+    # ## ---- Linear vectoring mode ---- ##
+    # print(f"\n\n<Case4> Linear vectoring mode")
+    # # z + y/x
+    # x = 1
+    # y = 2.46519033e-32
+    # z = 0
+    # x_cordic, y_remain, div_cordic, convergence_list = cordic(x, y, z, m=0, iterations=32, mode='vectoring')
+    # print(f"x: {x_cordic}, y: {y_remain}, div: {div_cordic}")
+
+    # div_true = z + y / x
+
+    # div_error = abs(div_cordic - div_true)
+    # print(f"Error:  div error={div_error}")
+
+
+    # ## ---- Hyperbolic rotation mode ---- ##
     print(f"\n\n<Case5> Hyperbolic rotation mode")
-    # coshz + sinhz = exp(z)
-    exp1_cordic, exp2_cordic, theta_remain, convergence_list = cordic(1, 1, 0.4, m=-1, iterations=32, mode='rotation')
-    print(f"exp1(z): {exp1_cordic}, exp2(z): {exp1_cordic}, theta: {theta_remain}")
-    exp_true = math.exp(0.4)
+    # # coshz + sinhz = exp(z)
+    # exp1_cordic, exp2_cordic, theta_remain, convergence_list = cordic(1, 1, 0.4, m=-1, iterations=32, mode='rotation')
+    # print(f"exp1(z): {exp1_cordic}, exp2(z): {exp1_cordic}, theta: {theta_remain}")
+    # exp_true = math.exp(0.4)
 
-    exp_error = abs(exp1_cordic - exp_true)
-    print(f"Error: exp(z) error={exp_error}")
+    # exp_error = abs(exp1_cordic - exp_true)
+    # print(f"Error: exp(z) error={exp_error}")
 
+    value = -1.2
+    frac_part, int_part = np.modf(value)
+    e_int = 2**(int_part * 1.4426950408889634)  # log2(e) ≈ 1.4426950408889634
 
-    exp1_cordic, exp2_cordic, _, _ = cordic(1, 0, -70, m=-1, iterations=64, mode='rotation')
-    exp_approx = exp1_cordic + exp2_cordic
+    exp1_cordic, exp2_cordic, theta_remain, convergence_list = cordic(e_int, e_int, frac_part, m=-1, iterations=32, mode='rotation')
+    print(f"exp1(z): {exp1_cordic}, exp2(z): {exp2_cordic}, theta: {theta_remain}")
 
-    exp_true = math.exp(-70)
-    error = abs(exp_approx - exp_true)
+    exp_true = math.exp(value)
+    error = abs(exp1_cordic - exp_true)
 
-    print(f"CORDIC exp(-70) = {exp_approx}")
+    print(f"CORDIC exp(-70) = {exp1_cordic}")
     print(f"True exp(-70)   = {exp_true}")
     print(f"Error           = {error}")
     
 
-    ## ---- Hyperbolic vectoring mode ---- ##
-    print(f"\n\n<Case6> Hyperbolic rotation mode")
-    # square(x^2 - y^2), tanh-1(y)
-    square_cordic, y_remain, tanh_cordic, convergence_list = cordic(4, 2, 0, m=-1, iterations=32, mode='vectoring')
-    print(f"square: {square_cordic}, y: {y_remain}, tanh: {tanh_cordic}")
+    # ## ---- Hyperbolic vectoring mode ---- ##
+    # print(f"\n\n<Case6> Hyperbolic rotation mode")
+    # # square(x^2 - y^2), tanh-1(y)
+    # square_cordic, y_remain, tanh_cordic, convergence_list = cordic(4, 2, 0, m=-1, iterations=32, mode='vectoring')
+    # print(f"square: {square_cordic}, y: {y_remain}, tanh: {tanh_cordic}")
 
-    square_true = math.sqrt(4**2 - 2**2)  # sqrt(2^2 - 4^2)
-    tanh_true = math.atanh(2/4) # 0.549... ≈ tanh^-1(0.5)
+    # square_true = math.sqrt(4**2 - 2**2)  # sqrt(2^2 - 4^2)
+    # tanh_true = math.atanh(2/4) # 0.549... ≈ tanh^-1(0.5)
 
-    square_error = abs(square_cordic - square_true)
-    tanh_error = abs(tanh_cordic - tanh_true)
-    print(f"Error:  square error={square_error}, tanh error={tanh_error}")
+    # square_error = abs(square_cordic - square_true)
+    # tanh_error = abs(tanh_cordic - tanh_true)
+    # print(f"Error:  square error={square_error}, tanh error={tanh_error}")
 
 
     # Plot convergence

NAF_unit.py
@@ -82,8 +82,10 @@ def SoftMax(x):
     maximum = np.max(x)
 
     diff = x - maximum
+    print("Diff Golden:", diff)
 
     exp_diff = np.exp(diff)
+    print("Exp. diff Golden:", exp_diff)
 
     summation = np.sum(exp_diff)
 
@@ -178,21 +180,23 @@ def SoftMax_3(x):
     diff = x - maximum
     
     # cordic approximation of exponentials
-    log2e = 1.5
-    diff_log2 = diff * log2e
-    exp_diff = cordic_pow2(diff_log2)
-    # exp_diff_list = []
-    # for d in diff:
-    #     cosh_val, sinh_val, _, _ = cordic(1, 0, d, m=-1, iterations=32, mode='rotation')
-    #     expx_val = cosh_val + sinh_val  # exp(x) = cosh(x) + sinh(x)
-    #     exp_diff_list.append(expx_val)  # exp ≈ cosh + sinh = x + y
+    log2e = 1.442695
+    diff_frac_part, diff_int_part = np.modf(diff)
 
-    # exp_diff = np.array(exp_diff_list)
+    exp_diff_list = []
+
+    for i in range(len(diff)):
+        e_int = 2**(diff_int_part[i] * log2e)  # TODO: the odd cannot shift, need to used LUT
+
+        exp_diff, _, _, _ = cordic(e_int, e_int, diff_frac_part[i], m=-1, iterations=32, mode='rotation')
+
+        # exp_diff_list.append(e_int * e_frac)
+        exp_diff_list.append(exp_diff)
+
+    exp_diff = np.array(exp_diff_list)
     
-    # print("diff:", diff)
-    # print("Exp. diff Cordic:", exp_diff)
-    # print("Exp. diff Golden:", np.exp(diff))
-
+    print("diff:", diff)
+    print("Exp. diff Cordic:", exp_diff)
     summation = np.sum(exp_diff)
 
     # cordic approximation of inverse